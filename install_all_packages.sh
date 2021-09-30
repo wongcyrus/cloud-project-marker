bec9dd008d4b6277a7f9ab0522ac4b754ed48f49
EC2 Default User <ec2-user@ip-172-31-86-12.ec2.internal>
2021-09-30 08:24:46 +0000

    update packages

--- install_all_packages.sh
+++ install_all_packages.sh
@@ -1,2 +1,2 @@
-find . -name package.json -not -path "*/node_modules/*" -not -path "*/cdk.out/*" -exec bash -c "npm --prefix \$(dirname {}) i" \;
+find . -name package.json -not -path "*/node_modules/*" -not -path "*/cdk.out/*" -not -path "*/.aws-sam/*" -exec bash -c "npm --prefix \$(dirname {}) i" \;
 npm run build
